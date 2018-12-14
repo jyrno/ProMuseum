cc4802cc9098cbc663a625e27c2faa46a0a7bf4e
Bahdan Yanovich <bahdan.yanovich@proekspert.ee>
2018-12-14 08:54:37 +0200

    plate item

--- app/src/main/java/ee/proekspert/promuseum/datasource/ItemProvider.java
+++ app/src/main/java/ee/proekspert/promuseum/datasource/ItemProvider.java
@@ -24,17 +24,21 @@ public class ItemProvider {
 
     private void initAllItems() {
         Item.ItemLocation locationMuseumKumu = new Item.ItemLocation("KUMU");
-        Item.ItemLocation locationStorageKumuMainStorage = new Item.ItemLocation("KUMU / Main Storage");
+        Item.ItemLocation garage48Shelve = new Item.ItemLocation("Palo Alto / GARAGE48 Riiul");
 
-        Item item = new Item("0201472125", "TKM 38951 L34", "e0ce0449-8156-413b-afb1-b934ee78e888", "Estonia kontserdisaali lühter", "Arhitekt Alar Kotli", "Kahjustusi ei ole", Item.ItemStatus.UNCHECKED, Item.ItemState.SATISFACTORY, locationMuseumKumu);
+        Item item = new Item("0201472125", "TKM 38951 L34", "e0ce0449-8156-413b-afb1-b934ee78e888", "Estonia kontserdisaali lühter", "Arhitekt Alar Kotli", "Kahjustusi ei ole", Item.ItemStatus.UNCHECKED, Item.ItemState.SATISFACTORY, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
         allItems.add(item);
 
-        item = new Item("0201699391", "TKM 38952 L34", "3ccfa93f-322d-4fc6-b40b-22cc6d9a490f", "Liv-, Est- und Curlaendisches Privatrecht", "pealkirja keel: saksa keel , ainese keel: saksa keel", "Veidi mõranenud külgede pealt", Item.ItemStatus.UNCHECKED, Item.ItemState.SATISFACTORY, locationMuseumKumu);
+        item = new Item("0201699391", "TKM 38952 L34", "3ccfa93f-322d-4fc6-b40b-22cc6d9a490f", "Liv-, Est- und Curlaendisches Privatrecht", "pealkirja keel: saksa keel , ainese keel: saksa keel", "Veidi mõranenud külgede pealt", Item.ItemStatus.UNCHECKED, Item.ItemState.SATISFACTORY, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
         allItems.add(item);
 
-        item = new Item("0202444499", "TKM 38953 L34", "23f854dd-fe23-46e7-9156-b6de07889e2a", "Tass. KALEVIPOJA-ainelise portselanmaaliga", "This is very cool stuff", "", Item.ItemStatus.UNCHECKED, Item.ItemState.GOOD, locationMuseumKumu);
+        item = new Item("0202444499", "TKM 38953 L34", "23f854dd-fe23-46e7-9156-b6de07889e2a", "Tass. KALEVIPOJA-ainelise portselanmaaliga", "This is very cool stuff", "", Item.ItemStatus.UNCHECKED, Item.ItemState.GOOD, garage48Shelve);
+        Log.w("ItemProvider", "InitAllItems: item " + item);
+        allItems.add(item);
+
+        item = new Item("0201198362", "TKM 38953 L34", "feef0e32-5109-473d-a84d-6a044465b58f", "Taldrik", "Carl Cristian Ficki fajansimanufaktuur", "", Item.ItemStatus.UNCHECKED, Item.ItemState.SATISFACTORY, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
         allItems.add(item);
 
@@ -42,31 +46,31 @@ public class ItemProvider {
         Log.w("ItemProvider", "InitAllItems: item " + item);
         allItems.add(item);
 
-        item = new Item("0223456", "TKM 38955 L34", "613eae22-47fc-495d-b031-c539b78b7c67", "Very Bad Painting", "This is very bad stuff", "Sooo damaged", Item.ItemStatus.UNCHECKED, Item.ItemState.VERY_BAD, locationStorageKumuMainStorage);
+        item = new Item("0223456", "TKM 38955 L34", "613eae22-47fc-495d-b031-c539b78b7c67", "Very Bad Painting", "This is very bad stuff", "Sooo damaged", Item.ItemStatus.UNCHECKED, Item.ItemState.VERY_BAD, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
 
         allItems.add(item);
-        item = new Item("0234567", "TKM 38956 L34", "8984c4e4-3829-4c5d-a887-4962ced71ca0","Okeish Paining", "This is quite okeish stuff", "Just old", Item.ItemStatus.UNCHECKED, Item.ItemState.SATISFACTORY, locationStorageKumuMainStorage);
+        item = new Item("0234567", "TKM 38956 L34", "8984c4e4-3829-4c5d-a887-4962ced71ca0","Okeish Paining", "This is quite okeish stuff", "Just old", Item.ItemStatus.UNCHECKED, Item.ItemState.SATISFACTORY, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
 
         allItems.add(item);
-        item = new Item("0245678", "TKM 38957 L34", "a7468f01-1a1e-48d5-9739-d4408c9ef500","Lost Painting", "This was very cool but got lost", "No idea as it is lost", Item.ItemStatus.LOST, Item.ItemState.VERY_GOOD, locationStorageKumuMainStorage);
+        item = new Item("0245678", "TKM 38957 L34", "a7468f01-1a1e-48d5-9739-d4408c9ef500","Lost Painting", "This was very cool but got lost", "No idea as it is lost", Item.ItemStatus.LOST, Item.ItemState.VERY_GOOD, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
 
         allItems.add(item);
-        item = new Item("0256789", "TKM 38958 L34", "c5946e2a-d971-4149-b01e-1c28e6c67826","Dummy Painting", "no description", "no damage", Item.ItemStatus.CHECKED, Item.ItemState.GOOD, locationStorageKumuMainStorage);
+        item = new Item("0256789", "TKM 38958 L34", "c5946e2a-d971-4149-b01e-1c28e6c67826","Dummy Painting", "no description", "no damage", Item.ItemStatus.CHECKED, Item.ItemState.GOOD, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
 
         allItems.add(item);
-        item = new Item("0201472", "TKM 38959 L34", "ec9afd14-f8ec-423c-958c-5c166652a305","Old USSR Poster", "From the 90s", "gonna tear soon", Item.ItemStatus.CHECKED, Item.ItemState.BAD, locationStorageKumuMainStorage);
+        item = new Item("0201472", "TKM 38959 L34", "ec9afd14-f8ec-423c-958c-5c166652a305","Old USSR Poster", "From the 90s", "gonna tear soon", Item.ItemStatus.CHECKED, Item.ItemState.BAD, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
 
         allItems.add(item);
-        item = new Item("0202585", "TKM 38960 L34", "d5646dbc-46f8-4c2e-b715-12d63674a5f8","Portrey of one Unknown Italian landlord ", "Unknown author", "no damage", Item.ItemStatus.UNCHECKED, Item.ItemState.GOOD, locationStorageKumuMainStorage);
+        item = new Item("0202585", "TKM 38960 L34", "d5646dbc-46f8-4c2e-b715-12d63674a5f8","Portrey of one Unknown Italian landlord ", "Unknown author", "no damage", Item.ItemStatus.UNCHECKED, Item.ItemState.GOOD, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
 
         allItems.add(item);
-        item = new Item("0203698", "TKM 38961 L34", "cb74814c-cf8c-44ff-b6ca-15ed41ffac15","Estonian Landscapes", "Journal from the exhibition", "", Item.ItemStatus.UNCHECKED, Item.ItemState.VERY_GOOD, locationStorageKumuMainStorage);
+        item = new Item("0203698", "TKM 38961 L34", "cb74814c-cf8c-44ff-b6ca-15ed41ffac15","Estonian Landscapes", "Journal from the exhibition", "", Item.ItemStatus.UNCHECKED, Item.ItemState.VERY_GOOD, garage48Shelve);
         Log.w("ItemProvider", "InitAllItems: item " + item);
 
         allItems.add(item);
